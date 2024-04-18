ec1ff72cd1bb67c160e3fd22a89bfd75e3e80c65
rc760 <rcampuzano@ucsd.edu>
2024-04-18 14:16:24 -0700

    initial code submission
    Attempt at extra credit

--- main.py
+++ main.py
@@ -13,7 +13,15 @@ led_pins = [
     board.IO21,
     board.IO26, # type: ignore
     board.IO47,
-    # do the rest...
+    board.IO33, # type: ignore
+    board.IO34, # type: ignore
+    board.IO48,
+    board.IO35,
+    board.IO36,
+    board.IO37,
+    board.IO38,
+    board.IO39,
+    board.IO40,
 ]
 
 leds = [DigitalInOut(pin) for pin in led_pins]
@@ -26,11 +34,74 @@ while True:
     volume = microphone.value
 
     print(volume)
+    if volume > 23500:
+        leds[0].value = 1
+    elif volume < 23000:
+        leds[0].value = 0
 
-    leds[0].value = not leds[0].value
-    leds[1].value = not leds[0].value
 
-    sleep(1)
+    if volume > 23750:
+        leds[1].value = 1
+    elif volume < 22500:
+        leds[1].value = 0
+
+
+    if volume > 24000:
+        leds[2].value = 1
+    elif volume < 23000:
+        leds[2].value = 0
+
+
+    if volume > 24250:
+        leds[3].value = 1
+    elif volume < 23000:
+        leds[3].value = 0
+
+
+    if volume > 24500:
+        leds[4].value = 1
+    elif volume < 23000:
+        leds[4].value = 0
+
+
+    if volume > 24750:
+        leds[5].value = 1
+    elif volume < 23000:
+        leds[5].value = 0
+
+
+    if volume > 25000:
+        leds[6].value = 1
+    elif volume < 23000:
+        leds[6].value = 0
+
+
+    if volume > 26500:
+        leds[7].value = 1
+    elif volume < 23000:
+        leds[7].value = 0
+
+
+    if volume > 27250:
+        leds[8].value = 1
+    elif volume < 23000:
+        leds[8].value = 0
+
+
+    if volume > 29000:
+        leds[9].value = 1
+    elif volume < 23000:
+        leds[9].value = 0
+
+
+    if volume > 31000:
+        leds[10].value = 1
+    elif volume < 23000:
+        leds[10].value = 0
+
+
+
+    sleep(.05)
 
     # instead of blinking,
     # how can you make the LEDs
